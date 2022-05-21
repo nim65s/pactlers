8cc87a6d0c3ccd94f81112a4596fe677a6616593
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-05-21 18:25:09 +0200

    usb
    ref https://github.com/stm32-rs/stm32f1xx-hal/blob/master/examples/usb_serial.rs

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,12 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use cortex_m::asm::delay;
 use rtt_target::{rprintln, rtt_init_print};
+use stm32f1xx_hal::usb::{Peripheral, UsbBus};
 use stm32f1xx_hal::{adc, pac, prelude::*};
+use usb_device::prelude::*;
+use usbd_serial::{SerialPort, USB_CLASS_CDC};
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
@@ -19,7 +23,15 @@ fn main() -> ! {
     // clock is configurable. So its frequency may be tweaked to meet certain
     // practical needs. User specified value is be approximated using supported
     // prescaler values 2/4/6/8.
-    let clocks = rcc.cfgr.adcclk(2.MHz()).freeze(&mut flash.acr);
+    let clocks = rcc
+        .cfgr
+        .adcclk(2.MHz())
+        .use_hse(8.MHz())
+        .sysclk(48.MHz())
+        .pclk1(24.MHz())
+        .freeze(&mut flash.acr);
+
+    assert!(clocks.usbclk_valid());
 
     // Setup ADC
     let mut adc1 = adc::Adc::adc1(p.ADC1, clocks);
@@ -30,9 +42,41 @@ fn main() -> ! {
     // Configure pb0, pb1 as an analog input
     let mut ch0 = gpioa.pa0.into_analog(&mut gpioa.crl);
 
+    // BluePill board has a pull-up resistor on the D+ line.
+    // Pull the D+ pin down to send a RESET condition to the USB bus.
+    // This forced reset is needed only for development, without it host
+    // will not reset your device when you upload new firmware.
+    let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.crh);
+    usb_dp.set_low();
+    delay(clocks.sysclk().raw() / 100);
+
+    let usb = Peripheral {
+        usb: p.USB,
+        pin_dm: gpioa.pa11,
+        pin_dp: usb_dp.into_floating_input(&mut gpioa.crh),
+    };
+    let usb_bus = UsbBus::new(usb);
+
+    let mut serial = SerialPort::new(&usb_bus);
+
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .manufacturer("Fake company")
+        .product("Serial port")
+        .serial_number("TEST")
+        .device_class(USB_CLASS_CDC)
+        .build();
+
+    let mut buf = [0u8; 2];
     loop {
+        if !usb_dev.poll(&mut [&mut serial]) {
+            continue;
+        }
         let data: u16 = adc1.read(&mut ch0).unwrap();
-        rprintln!("adc1: {}", data);
+        [buf[0], buf[1]] = data.to_le_bytes();
+        let count = serial.write(&buf).unwrap();
+        if count != 2 {
+            rprintln!("warning: {} byte written", count);
+        }
     }
 }
 
