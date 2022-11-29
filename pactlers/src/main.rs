3fe33acafd639644d377be027fc7b2be51e9d5d9
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-11-29 19:43:25 +0100

    tokio

--- pactlers/src/main.rs
+++ pactlers/src/main.rs
@@ -1,5 +1,7 @@
-use std::{sync::mpsc, thread, time::Duration};
+use async_channel::unbounded;
+use std::time::Duration;
 use std::io::Read;
+use tokio::{task, time::sleep};
 
 mod pactl;
 
@@ -8,10 +10,11 @@ use crate::pactl::*;
 const N_ADCS: usize = 5;
 const HEADER: [u8; 4] = [0xFF, 0xFF, 0xFD, 0];
 
-fn main() {
-    let (tx, rx) = mpsc::channel();
+#[tokio::main]
+async fn main() {
+    let (tx, rx) = unbounded();
 
-    thread::spawn(move || {
+    task::spawn(async move {
         let chans: [PactlChannel; N_ADCS] = [
             PactlChannel::new(PactlClass::Spk, "HDA"),
             PactlChannel::new(PactlClass::Mic, "BIRD"),
@@ -22,16 +25,15 @@ fn main() {
         let mut buf: [u8; 3] = [0; 3];
         let mut last: [u8; 3] = [0; 3];
         loop {
-            while let Ok(b) = rx.try_recv() {
+            while let Ok(b) = rx.recv().await {
                 buf = b;
             }
             if buf != last {
                 let v = u32::from_le_bytes([buf[2], buf[1], 0, 0]);
                 chans[buf[0] as usize].set(v);
-                //println!("{}: {}", buf[0], v);
                 last = buf;
             }
-            thread::sleep(Duration::from_millis(1));
+            sleep(Duration::from_millis(1)).await;
         }
     });
 
@@ -42,7 +44,7 @@ fn main() {
             .timeout(Duration::from_secs(3600 * 24 * 7))
             .open();
         if port.is_err() {
-            thread::sleep(Duration::from_millis(500));
+            sleep(Duration::from_millis(500)).await;
             continue;
         }
         println!("Connected.");
@@ -69,7 +71,7 @@ fn main() {
                     buf[buffer_index] = byte;
                     buffer_index += 1;
                     if buffer_index == buf.len() {
-                        tx.send(buf).unwrap();
+                        tx.send(buf).await.unwrap();
                         //println!("ok: {:?}", buf);
                         header_index = 0;
                     }
